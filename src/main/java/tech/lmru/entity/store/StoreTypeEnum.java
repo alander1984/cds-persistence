e698e50dda48d6d6da66e23887171c9a636e9ee2
Yaroslav Korzhev <ykorzhev@egartech.com>
2019-04-05 16:49:31 +0000

    store type enum

--- src/main/java/tech/lmru/entity/store/StoreTypeEnum.java
+++ src/main/java/tech/lmru/entity/store/StoreTypeEnum.java
@@ -1,6 +1,32 @@
 package tech.lmru.entity.store;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum StoreTypeEnum {
-    OFFLINE,
-    ONLINE
+    OFFLINE(0),
+    ONLINE(1);
+    
+    private int intValue;
+    
+    private static Map<Integer, StoreTypeEnum> map = new HashMap<Integer, StoreTypeEnum>();
+    
+    static {
+        for( StoreTypeEnum storeTypeEnum : StoreTypeEnum.values()) {
+            map.put(storeTypeEnum.intValue, storeTypeEnum);
+        }
+    }
+    
+    private StoreTypeEnum(final int enumIntValue) {
+        intValue = enumIntValue;
+    }
+    
+    public static StoreTypeEnum valueOf(int enumNumber) {
+        return map.get(enumNumber);
+    }
+    
+    private int getIntValue() {
+        return intValue;
+    }
+    
 }
