a447fc69abb95eb21de2cf1d418dd83df5bc5dba
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-04-18 10:45:11 +0000

    add fields in Delivery

--- src/main/java/tech/lmru/entity/order/Delivery.java
+++ src/main/java/tech/lmru/entity/order/Delivery.java
@@ -72,11 +72,23 @@ public class Delivery {
     @JsonFormat(pattern = "yyyy-mm-dd HH:MM")
     private LocalDateTime deliveryDateMax;
     
+    @Column(name="fullname")
+    private String fullName;
+    
     @Column(name="comment")
     private String comment;
     
+    @Column(name="external")
+    private String external;
+    
     @Column(name="city")
     private String city;
+    
+    @Column(name="zone")
+    private String zone;
+    
+    @Column(name="metro_station")
+    private String metroStation;
     
     @Column(name="street")
     private String street;
@@ -87,8 +99,35 @@ public class Delivery {
     @Column(name="entrance")
     private String entrance;
     
+    @Column(name="floor")
+    private String floor;
+    
     @Column(name="flat")
     private String flat;
+    
+    @Column(name="company_description")
+    private String company;
+    
+    @Column(name="consignee")
+    private String consignee;
+    
+    @Column(name="phone_primary")
+    private String phone;
+    
+    @Column(name="phone_secondary")
+    private String phoneSecondary;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="unloadtype")
+    private String unloadType;
+    
+    @Column(name="paper")
+    private String paper;
+    
+    @Column(name="payment_status")
+    private String paymentStatus;
     
     
     @OneToMany(mappedBy="delivery", fetch = FetchType.EAGER)
@@ -365,7 +404,189 @@ public class Delivery {
 		this.store = store;
 	}
 
-    
+    /**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+
+	/**
+	 * @param fullName the fullName to set
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	/**
+	 * @return the external
+	 */
+	public String getExternal() {
+		return external;
+	}
+
+	/**
+	 * @param external the external to set
+	 */
+	public void setExternal(String external) {
+		this.external = external;
+	}
+	
+	/**
+	 * @return the zone
+	 */
+	public String getZone() {
+		return zone;
+	}
+
+	/**
+	 * @param zone the zone to set
+	 */
+	public void setZone(String zone) {
+		this.zone = zone;
+	}
+	
+	/**
+	 * @return the metroStation
+	 */
+	public String getMetroStation() {
+		return metroStation;
+	}
+
+	/**
+	 * @param metroStation the metroStation to set
+	 */
+	public void setMetroStation(String metroStation) {
+		this.metroStation = metroStation;
+	}
+	
+	/**
+	 * @return the floor
+	 */
+	public String getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor the floor to set
+	 */
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+	
+	/**
+	 * @return the company
+	 */
+	public String getCompany() {
+		return company;
+	}
+
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	/**
+	 * @return the phoneSecondary
+	 */
+	public String getPhoneSecondary() {
+		return phoneSecondary;
+	}
+
+	/**
+	 * @param phoneSecondary the phoneSecondary to set
+	 */
+	public void setPhoneSecondary(String phoneSecondary) {
+		this.phoneSecondary = phoneSecondary;
+	}
+	
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	/**
+	 * @return the unloadType
+	 */
+	public String getUnloadType() {
+		return unloadType;
+	}
+
+	/**
+	 * @param unloadType the unloadType to set
+	 */
+	public void setUnloadType(String unloadType) {
+		this.unloadType = unloadType;
+	}
+	
+	/**
+	 * @return the paper
+	 */
+	public String getPaper() {
+		return paper;
+	}
+
+	/**
+	 * @param paper the paper to set
+	 */
+	public void setPaper(String paper) {
+		this.paper = paper;
+	}
+	
+	/**
+	 * @return the paymentStatus
+	 */
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	/**
+	 * @param paymentStatus the paymentStatus to set
+	 */
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	
+	/**
+	 * @return the consignee
+	 */
+	public String getConsignee() {
+		return consignee;
+	}
+
+	/**
+	 * @param consignee the consignee to set
+	 */
+	public void setConsignee(String consignee) {
+		this.consignee = consignee;
+	}
+	
+	
     
 
 }
