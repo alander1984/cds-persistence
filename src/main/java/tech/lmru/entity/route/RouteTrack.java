2fedc6c5aae878fab80568041cf2776e3ebb1fac
porterinator <porterinator@gmail.com>
2019-04-25 07:59:59 +0000

    CDS-86

---
+++ src/main/java/tech/lmru/entity/route/RouteTrack.java
@@ -0,0 +1,85 @@
+package tech.lmru.entity.route;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROUTE_TRACK")
+public class RouteTrack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "route_track_seq")
+    @SequenceGenerator(name = "route_track_seq", sequenceName = "route_track_sequence", allocationSize = 1)
+    private long id;
+
+    @Column(name = "timestamp")
+    private long timestamp;
+
+    @Column(name = "lat")
+    private double lat;
+
+    @Column(name = "lon")
+    private double lon;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the timestamp
+	 */
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * @param timestamp the timestamp to set
+	 */
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	/**
+	 * @return the lat
+	 */
+	public double getLat() {
+		return lat;
+	}
+
+	/**
+	 * @param lat the lat to set
+	 */
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	/**
+	 * @return the lon
+	 */
+	public double getLon() {
+		return lon;
+	}
+
+	/**
+	 * @param lon the lon to set
+	 */
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+    
+
+}
